e67a40bfecaee29a3c5876e52bcc24ff40031832
Haider ali <75686258+Haider-Ali-Dev@users.noreply.github.com>
2021-07-18 23:53:57 +0400

    Update main.py

--- main.py
+++ main.py
@@ -24,20 +24,6 @@ def write_to_csv(data):
         w = csv.writer(f, delimiter=',', quotechar='"',  quoting=csv.QUOTE_MINIMAL)
         w.writerow([email, subject, message])
 
-# def reply_to_res(data):
-#     resp = data['email']
-#     subject = data['subject']
-#     message = data['message']
-#     email = EmailMessage()
-#     email['from'] = 'haiderdev.com'
-#     email['to'] = resp
-#     email['subject'] = 'Thanks'
-#     email.set_content('Test ETst')
-#     with smtplib.SMTP('smtp.gmail.com', 587) as server:
-#         server.ehlo()
-#         server.starttls()
-#         server.login('pythonandhaider@gmail.com', 'Haider123')
-#         server.send_message(email)
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
